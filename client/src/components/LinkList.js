f2f26a0b459337a7856d988a783020444650ffa4
Daniel Mehler <danieljmehler@gmail.com>
2023-09-16 17:09:11 -0400

    Add initial LinkList and List components

---
+++ client/src/components/LinkList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from './Link';
+import { useQuery, gql } from '@apollo/client';
+
+const LINKS_QUERY = gql`
+  {
+    links {
+      title
+      address
+      id
+    }
+  }
+`
+
+const LinkList = () => {
+  const { data } = useQuery(LINKS_QUERY)
+  return (
+    <div>
+      <h1>Links</h1>
+      {data && (
+	<>
+	  {data.links.map((link) => (
+            <Link key={link.id} link={link} />
+          ))}
+	</>
+      )}
+    </div>
+  );
+};
+
+export default LinkList;
