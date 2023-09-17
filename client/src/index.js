0e0b4ac7968455bcde79d31b738e161ff9d4f8fa
Daniel Mehler <danieljmehler@gmail.com>
2023-09-17 07:43:41 -0400

    Add BrowserRouter. Change document.getElementById to latest impl.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 // 1
 import {
@@ -24,10 +24,13 @@ const client = new ApolloClient({
 });
 
 // 4
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container)
+
+root.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
 );
- 
